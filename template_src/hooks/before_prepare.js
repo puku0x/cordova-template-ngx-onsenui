c07fda39a9380c2146b0e9f81b6246f794f25140
puku0x <puku0x@gmail.com>
2018-04-17 00:23:46 +0900

    remove before_prepare.js

--- template_src/hooks/before_prepare.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const execSync = require('child_process').execSync;
-
-module.exports = function(context) {
-  console.log('Building application...');
-  console.log(execSync("npm run build").toString('utf8'));
-};
